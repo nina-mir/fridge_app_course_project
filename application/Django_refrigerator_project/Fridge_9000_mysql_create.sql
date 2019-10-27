81ba2ba8552d886ca97c4cc6eeeb135238964355
Abhis33 <36435796+Abhis33@users.noreply.github.com>
2019-10-26 21:05:31 -0800

    Minor changes and populated db

---
+++ application/Django_refrigerator_project/Fridge_9000_mysql_create.sql
@@ -0,0 +1,82 @@
+USE 'refrigerator_development'
+CREATE TABLE `Users` (
+	`UserID` INT NOT NULL AUTO_INCREMENT,
+	`Email` varchar(255) NOT NULL UNIQUE,
+	`Username` varchar(255) NOT NULL UNIQUE,
+	`Password` varchar(255) NOT NULL,
+	`Name` varchar(255) NOT NULL,
+	`OwnedFridges` TEXT,
+	`FriendedFridges` TEXT,
+	`PersonalNotes` TEXT,
+	`eff_bgn_ts` DATETIME NOT NULL,
+	`eff_end_ts` DATETIME NOT NULL,
+	PRIMARY KEY (`UserID`,`Email`)
+);
+
+CREATE TABLE `Fridge` (
+	`FridgeID` INT NOT NULL AUTO_INCREMENT,
+	`FridgeName` varchar(255) NOT NULL,
+	`Owner` INT NOT NULL,
+	`Friends` TEXT NOT NULL,
+	`Auto_gen_grocery_list` TEXT,
+	`Manually_added_list` TEXT,
+	`creation_date` DATETIME NOT NULL,
+	`modified_date` DATETIME NOT NULL,
+	`eff_bgn_ts` DATETIME NOT NULL,
+	`eff_end_ts` DATETIME NOT NULL,
+	PRIMARY KEY (`FridgeID`)
+);
+
+CREATE TABLE `Items` (
+	`ItemID` INT NOT NULL AUTO_INCREMENT,
+	`ItemName` varchar(255) NOT NULL,
+	`Age` TIME NOT NULL,
+	`isPerishable` BOOLEAN NOT NULL,
+	`Calories` INT,
+	`creation_date` DATETIME NOT NULL,
+	`modified_date` DATETIME NOT NULL,
+	`eff_bgn_ts` DATETIME NOT NULL,
+	`eff_end_ts` DATETIME NOT NULL,
+	PRIMARY KEY (`ItemID`)
+);
+
+CREATE TABLE `Fridge_Contents` (
+	`FridgeID` INT NOT NULL,
+	`ItemID` INT NOT NULL,
+	`AddedBy` INT NOT NULL,
+	`ExpirationDate` DATETIME NOT NULL,
+	`Size` INT,
+	`creation_date` DATETIME NOT NULL,
+	`modified_date` DATETIME NOT NULL,
+	`eff_bgn_ts` DATETIME NOT NULL,
+	`eff_end_ts` DATETIME NOT NULL
+);
+
+CREATE TABLE `Recipes` (
+	`FridgeId` INT NOT NULL,
+	`UserId` INT NOT NULL,
+	`Title` TEXT NOT NULL,
+	`SourceUrl` TEXT NOT NULL,
+	`eff_bgn_ts` DATETIME NOT NULL,
+	`eff_end_ts` DATETIME NOT NULL
+);
+
+ALTER TABLE `Fridge` ADD CONSTRAINT `Fridge_fk0` FOREIGN KEY (`Owner`) REFERENCES `Users`(`UserID`);
+
+ALTER TABLE `Fridge_Contents` ADD CONSTRAINT `Fridge_Contents_fk0` FOREIGN KEY (`FridgeID`) REFERENCES `Fridge`(`FridgeID`);
+
+ALTER TABLE `Fridge_Contents` ADD CONSTRAINT `Fridge_Contents_fk1` FOREIGN KEY (`ItemID`) REFERENCES `Items`(`ItemID`);
+
+ALTER TABLE `Fridge_Contents` ADD CONSTRAINT `Fridge_Contents_fk2` FOREIGN KEY (`AddedBy`) REFERENCES `Users`(`UserID`);
+
+ALTER TABLE `Recipes` ADD CONSTRAINT `Recipes_fk0` FOREIGN KEY (`FridgeId`) REFERENCES `Fridge`(`FridgeID`);
+
+ALTER TABLE `Recipes` ADD CONSTRAINT `Recipes_fk1` FOREIGN KEY (`UserId`) REFERENCES `Users`(`UserID`);
+
+
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Apple','00:06:00',1,NULL,NOW(),NOW(),NOW(), '9999-12-31');
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Banana','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Orange','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Pork','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Pineapple','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
+insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Cheese','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
