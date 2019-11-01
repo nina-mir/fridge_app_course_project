9c3a8614cac054ef35651fd6bf359baf3fdc1e2e
Gangzhaorige <gangzhaorige.li@gmail.com>
2019-10-31 17:37:38 -0700

    Updated Database

--- Milestones/M2/Database Designs/Fridge_9000_mysql_create.sql
+++ Milestones/M2/Database Designs/Fridge_9000_mysql_create.sql
@@ -1,15 +1,16 @@
+-- Do migrate b4 running this thing code: docker-compose run web python manage.py inspectdb > refrigerator_app/models.py 
+-- create database ref_updated_10_31;
+-- use ref_updated_10_31;
+
 CREATE TABLE `Users` (
 	`UserID` INT NOT NULL AUTO_INCREMENT,
-	`Email` varchar(255) NOT NULL UNIQUE,
 	`Username` varchar(255) NOT NULL UNIQUE,
-	`Password` varchar(255) NOT NULL,
-	`Name` varchar(255) NOT NULL,
 	`OwnedFridges` TEXT,
 	`FriendedFridges` TEXT,
 	`PersonalNotes` TEXT,
 	`eff_bgn_ts` DATETIME NOT NULL,
 	`eff_end_ts` DATETIME NOT NULL,
-	PRIMARY KEY (`UserID`,`Email`)
+    PRIMARY KEY (`UserID`)
 );
 
 CREATE TABLE `Fridge` (
@@ -29,7 +30,7 @@ CREATE TABLE `Fridge` (
 CREATE TABLE `Items` (
 	`ItemID` INT NOT NULL AUTO_INCREMENT,
 	`ItemName` varchar(255) NOT NULL,
-	`Age` TIME NOT NULL,
+	`Age` BIGINT NOT NULL,
 	`isPerishable` BOOLEAN NOT NULL,
 	`Calories` INT,
 	`creation_date` DATETIME NOT NULL,
@@ -60,6 +61,8 @@ CREATE TABLE `Recipes` (
 	`eff_end_ts` DATETIME NOT NULL
 );
 
+ALTER TABLE `Users` ADD CONSTRAINT `Users_fk0` FOREIGN KEY (`username`) REFERENCES `auth_user`(`username`);
+
 ALTER TABLE `Fridge` ADD CONSTRAINT `Fridge_fk0` FOREIGN KEY (`Owner`) REFERENCES `Users`(`UserID`);
 
 ALTER TABLE `Fridge_Contents` ADD CONSTRAINT `Fridge_Contents_fk0` FOREIGN KEY (`FridgeID`) REFERENCES `Fridge`(`FridgeID`);
@@ -73,9 +76,3 @@ ALTER TABLE `Recipes` ADD CONSTRAINT `Recipes_fk0` FOREIGN KEY (`FridgeId`) REFE
 ALTER TABLE `Recipes` ADD CONSTRAINT `Recipes_fk1` FOREIGN KEY (`UserId`) REFERENCES `Users`(`UserID`);
 
 
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Apple','00:06:00',1,NULL,NOW(),NOW(),NOW(), '9999-12-31');
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Banana','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Orange','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Pork','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Pineapple','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
-insert into Items(ItemName, Age, isPerishable, Calories, creation_date, modified_date, eff_bgn_ts, eff_end_ts) values('Cheese','00:06:00',1,NULL,NOW(),NOW(),NOW(),'9999-12-31');
