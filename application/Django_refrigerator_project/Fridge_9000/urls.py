bbabc47a671e4d4c2aade071fad632e9d81353dd
nina-mir <ninamirf@gmail.com>
2019-11-08 14:03:17 -0800

    recipes_search.html making API call to food2fork working now

--- application/Django_refrigerator_project/Fridge_9000/urls.py
+++ application/Django_refrigerator_project/Fridge_9000/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('auth/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('search/', views.search, name='search'),
+    path('recipes/search', recipe_views.next, name='next'),
 ]
